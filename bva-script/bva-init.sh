8fa48e34a65b36b371f713913be2839b71a5323b
Bruno Moraes <bruno.moraes@NOTEPE05GB2Y.ativas.com.br>
2020-11-25 23:54:05 -0300

    My mistakes... misconfiguration

--- bva-script/bva-init.sh
+++ bva-script/bva-init.sh
@@ -1,6 +1,7 @@
 echo -e "\n$(date "+%d-%m-%Y --- %T") --- BVALAB config...\n"
 export PATH=$PATH:/usr/local/bin
 export NFS_SERVER=192.168.210.10
+yum install epel-release git ansible -y
 mkdir -p /bvalab/nfs ; mount -t nfs -vvvv ${NFS_SERVER}:/bvalab/nfs /bvalab/nfs
 echo '${NFS_SERVER}:/bvalab/nfs                 /bvalab/nfs              nfs          defaults    0       0' >> /etc/fstab
 grep -ir "PasswordAuthentication no" /etc/ssh/sshd_config | sed -i.bak 's/no/yes/g' /etc/ssh/sshd_config
@@ -8,11 +9,8 @@ systemctl restart sshd
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 swapoff -a
-yum install -y ansible telnet nfs-utils firewalld yum-utils epel-release git curl unzip wget yum-utils device-mapper-persistent-data lvm2 docker
-timedatectl set-timezone America/Sao_Paulo
 mkdir -p /etc/bvalab 
 git clone https://github.com/brunobva/vagrant-ansible.git /etc/bvalab/
-export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i /etc/bvalab/Ansible/inventory /etc/bvalab/Ansible/playbook.yml
 systemctl enable firewalld --now
 systemctl enable docker --now
