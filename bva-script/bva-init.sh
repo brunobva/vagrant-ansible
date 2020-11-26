321baab2d70945c37f0c964d330794caaeb59751
Bruno Moraes <bruno.moraes@NOTEPE05GB2Y.ativas.com.br>
2020-11-25 23:20:04 -0300

    New roles for Ansible - Init Server

--- bva-script/bva-init.sh
+++ bva-script/bva-init.sh
@@ -1,37 +1,22 @@
 echo -e "\n$(date "+%d-%m-%Y --- %T") --- BVALAB config...\n"
-setenforce 0
-sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+export PATH=$PATH:/usr/local/bin
+export NFS_SERVER=192.168.210.10
+mkdir -p /bvalab/nfs ; mount -t nfs -vvvv ${NFS_SERVER}:/bvalab/nfs /bvalab/nfs
+echo '${NFS_SERVER}:/bvalab/nfs                 /bvalab/nfs              nfs          defaults    0       0' >> /etc/fstab
 grep -ir "PasswordAuthentication no" /etc/ssh/sshd_config | sed -i.bak 's/no/yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 swapoff -a
-yum update -y && yum install -y kubelet kubeadm kubectl telnet nfs-utils firewalld yum-utils timedatectl epel-release git curl unzip wget yum-utils device-mapper-persistent-data lvm2 docker
+yum install -y ansible telnet nfs-utils firewalld yum-utils epel-release git curl unzip wget yum-utils device-mapper-persistent-data lvm2 docker
 timedatectl set-timezone America/Sao_Paulo
-export PATH=$PATH:/usr/local/bin
-export NFS_SERVER=192.168.210.10
-mkdir -p /bvalab/nfs ; mount -t nfs -vvvv ${NFS_SERVER}:/bvalab/nfs /bvalab/nfs
-echo '${NFS_SERVER}:/bvalab/nfs                 /bvalab/nfs              nfs          defaults    0       0' >> /etc/fstab
-mkdir -p /etc/docker
-mkdir -p /etc/bvalab ; 
+mkdir -p /etc/bvalab 
 git clone https://github.com/brunobva/vagrant-ansible.git /etc/bvalab/
-yum install -y ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i /etc/bvalab/Ansible/inventory /etc/bvalab/Ansible/playbook.yml
 systemctl enable firewalld --now
 systemctl enable docker --now
 systemctl enable kubelet --now
-sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 sudo systemctl daemon-reload
 sudo firewall-cmd --permanent --add-port={6443,2379,2380,10250,10251,10252,8200,8201,8500}/tcp ; firewall-cmd --reload
 echo -e "\n$(date "+%d-%m-%Y --- %T") --- BVALAB config...DONE\n"
